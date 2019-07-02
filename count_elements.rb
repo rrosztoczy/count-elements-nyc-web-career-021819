ca9af50257b6aa7459f66a2cf3a9a23dd43d4e49
rrosztoczy <rrosztoczy@gmail.com>
2019-07-02 10:29:34 -0600

    done

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,19 @@
 def count_elements(array)
-  # code goes here
+  count_hash = {}
+  # Iterate through array
+  array.each do |element|
+    #Check if element is in array, if it is not, add it
+    # k_element = element.to_sym
+    if count_hash[element].nil?
+      count_hash[element] = 1
+    else
+      count_hash[element] += 1
+    end
+  end
+  count_hash
 end
+
+# animals = ['tortoise', 'aye-aye', 'honey badger', 'aye-aye', 'tortoise', 'tortoise']
+# count_elements(animals)
+# # => {'tortoise' => 3, 'aye-aye' => 2, 'honey badger' => 1}
  
